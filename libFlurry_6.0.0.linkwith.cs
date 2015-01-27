7429a66f2181035fc0a4facfff4542f94cc5a347
Nezz <bloodsource@vipmail.hu>
2015-01-27 17:33:20 +0100

    Fixed linking (supports ARM64, but not ARMv7s)

--- libFlurry_6.0.0.linkwith.cs
+++ libFlurry_6.0.0.linkwith.cs
@@ -1,6 +1,6 @@
 using ObjCRuntime;
 
 [assembly: LinkWith ("libFlurry_6.0.0.a", 
-	LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.ArmV7s, 
+    LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Arm64, 
 	Frameworks = "Security SystemConfiguration",
 	ForceLoad = false)]
